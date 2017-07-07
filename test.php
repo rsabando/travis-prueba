afb521872b2bc022c9e80fad17064a00f6ebba33
rsabando <rsabando@espol.edu.ec>
2017-07-07 13:28:54 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,11 +4,6 @@ class Test extends PHPUnit_Framework_TestCase
   public function testOnePlusOne() {
     $this->assertEquals(1+1,2);
     }
-  
-  public function testFailingInclude()
-    {
-        include 'not_existing_file.php';
-    }
 
 }
 ?>
